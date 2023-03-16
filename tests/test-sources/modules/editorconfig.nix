02d23fbeb1314b413979bb80cb2db8ffee06e70a
traxys <quentin+dev@familleboyer.net>
2023-03-16 22:31:44 +0100

    editorconfig: Add options to configure builtin editorconfig

---
+++ tests/test-sources/modules/editorconfig.nix
@@ -0,0 +1,16 @@
+{
+  disable = {
+    editorconfig.enable = false;
+  };
+
+  example = {
+    editorconfig.properties.foo = ''
+      function(bufnr, val, opts)
+        if opts.charset and opts.charset ~= "utf-8" then
+          error("foo can only be set when charset is utf-8", 0)
+        end
+        vim.b[bufnr].foo = val
+      end
+    '';
+  };
+}
